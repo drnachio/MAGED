347f0306078ddee2fc674e82d92a113d17332c50
Mgonand <marcos@binpar.com>
2015-10-11 21:08:15 +0200

    fixes cuads

--- App/server/fixtures.js
+++ App/server/fixtures.js
@@ -1,6 +1,139 @@
 /**
  * Created by Marcos on 10/10/2015.
  */
+
+let addSheets = function(cArray, sheets){
+    for(let i = 0; i < 5; i++){
+        let type = '';
+        let rnd = Math.random();
+        if(rnd <= MAGED.Constants.RARITY.ULTRARARE){
+            let filt = _.filter(cArray, function(obj){
+                if(obj.rarity){
+                    return obj.rarity() === MAGED.Constants.RARITY.ULTRARARE;
+                }
+                else{
+                    return false;
+                }
+            });
+            if(filt.length > 0){
+                type = filt[Math.floor(Math.random() * filt.length)].name;
+            }
+            else{
+                type = cArray[Math.floor(Math.random() * cArray.length)].name;
+            }
+        }
+        else if(rnd <= MAGED.Constants.RARITY.VERYRARE){
+            let filt = _.filter(cArray, function(obj){
+                if(obj.rarity){
+                    return obj.rarity() === MAGED.Constants.RARITY.VERYRARE;
+                }
+                else{
+                    return false;
+                }
+            });
+            if(filt.length > 0){
+                type = filt[Math.floor(Math.random() * filt.length)].name;
+            }
+            else{
+                type = cArray[Math.floor(Math.random() * cArray.length)].name;
+            }
+        }
+        else if(rnd <= MAGED.Constants.RARITY.RARE){
+            let filt = _.filter(cArray, function(obj){
+                if(obj.rarity){
+                    return obj.rarity() === MAGED.Constants.RARITY.RARE;
+                }
+                else{
+                    return false;
+                }
+            });
+            if(filt.length > 0){
+                type = filt[Math.floor(Math.random() * filt.length)].name;
+            }
+            else{
+                type = cArray[Math.floor(Math.random() * cArray.length)].name;
+            }
+        }
+        else if(rnd <= MAGED.Constants.RARITY.INFREQUENT){
+            let filt = _.filter(cArray, function(obj){
+                if(obj.rarity){
+                    return obj.rarity() === MAGED.Constants.RARITY.INFREQUENT;
+                }
+                else{
+                    return false;
+                }
+            });
+            if(filt.length > 0){
+                type = filt[Math.floor(Math.random() * filt.length)].name;
+            }
+            else{
+                type = cArray[Math.floor(Math.random() * cArray.length)].name;
+            }
+        }
+        else if(rnd <= MAGED.Constants.RARITY.UNCOMMON){
+            let filt = _.filter(cArray, function(obj){
+                if(obj.rarity){
+                    return obj.rarity() === MAGED.Constants.RARITY.UNCOMMON;
+                }
+                else{
+                    return false;
+                }
+            });
+            if(filt.length > 0){
+                type = filt[Math.floor(Math.random() * filt.length)].name;
+            }
+            else{
+                type = cArray[Math.floor(Math.random() * cArray.length)].name;
+            }
+        }
+        else if(rnd <= MAGED.Constants.RARITY.COMMON){
+            let filt = _.filter(cArray, function(obj){
+                if(obj.rarity){
+                    return obj.rarity() === MAGED.Constants.RARITY.COMMON;
+                }
+                else{
+                    return false;
+                }
+            });
+            if(filt.length > 0){
+                type = filt[Math.floor(Math.random() * filt.length)].name;
+            }
+            else{
+                type = cArray[Math.floor(Math.random() * cArray.length)].name;
+            }
+        }
+        else{
+            let filt = _.filter(cArray, function(obj){
+                if(obj.rarity){
+                    return obj.rarity() === MAGED.Constants.RARITY.REGULAR;
+                }
+                else{
+                    return false;
+                }
+            });
+            if(filt.length > 0){
+                type = filt[Math.floor(Math.random() * filt.length)].name;
+            }
+            else{
+                type = cArray[Math.floor(Math.random() * cArray.length)].name;
+            }
+        }
+        sheets.push(MAGED.Classes[type].createNewObject({dynamic: Math.random() > 0.5, h: Math.round(Math.random()*10), b: 0}));
+    }
+    //order by rarity
+    sheets.sort(function(a,b){
+        return a.constructor.rarity() - b.constructor.rarity();
+    });
+    for(let i = 0; i < sheets.length; i++){
+        let b = 0;
+        if(i != 0) {
+            let prevSheet = sheets[i - 1];
+            b = prevSheet.b + prevSheet.h;
+        }
+        sheets[i].b = b;
+    }
+};
+
 if(MAGED.Collections.GameObjects.find().count() === 0){
     console.log('START!');
     let cArray = _.toArray(MAGED.Classes).filter(function(obj){
@@ -12,263 +145,23 @@ if(MAGED.Collections.GameObjects.find().count() === 0){
             let _y = -y;
             let _z = (x + y);
             let z = -_z;
-            let cell = MAGED.Classes.Cell.createNewObject({_x: x, _y:y,_z:z,_h:Math.round(Math.random()*100),_stack:[]});
-            let _cell = MAGED.Classes.Cell.createNewObject({_x: _x, _y:_y,_z:_z,_h:Math.round(Math.random()*100),_stack:[]});
-            let sheets = [];
-            let _sheets = [];
-            for(let i = 0; i < 5; i++){
-                let type = '';
-                let rnd = Math.random();
-                if(rnd <= MAGED.Constants.RARITY.ULTRARARE){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.ULTRARARE;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.VERYRARE){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.VERYRARE;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.RARE){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.RARE;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.INFREQUENT){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.INFREQUENT;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.UNCOMMON){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.UNCOMMON;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.COMMON){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.COMMON;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else{
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.REGULAR;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                let b = 0;
-                if(i != 0){
-                    let prevSheet =sheets[i-1];
-                    b = prevSheet.b + prevSheet.h;
-                }
-                sheets.push(MAGED.Classes[type].createNewObject({dynamic: Math.random() > 0.5, h: Math.round(Math.random()*10), b: b}));
-            }
-            for(let i = 0; i < 5; i++){
-                let type = '';
-                let rnd = Math.random();
-                if(rnd <= MAGED.Constants.RARITY.ULTRARARE){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.ULTRARARE;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.VERYRARE){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.VERYRARE;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.RARE){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.RARE;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.INFREQUENT){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.INFREQUENT;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.UNCOMMON){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.UNCOMMON;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else if(rnd <= MAGED.Constants.RARITY.COMMON){
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.COMMON;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                else{
-                    let filt = _.filter(cArray, function(obj){
-                        if(obj.rarity){
-                            return obj.rarity() === MAGED.Constants.RARITY.REGULAR;
-                        }
-                        else{
-                            return false;
-                        }
-                    });
-                    if(filt.length > 0){
-                        type = filt[Math.floor(Math.random() * filt.length)].name;
-                    }
-                    else{
-                        type = cArray[Math.floor(Math.random() * cArray.length)].name;
-                    }
-                }
-                let b = 0;
-                if(i != 0){
-                    let prevSheet =sheets[i-1];
-                    b = prevSheet.b + prevSheet.h;
-                }
-                _sheets.push(MAGED.Classes[type].createNewObject({dynamic: Math.random() > 0.5, h: Math.round(Math.random()*10), b: b}));
-            }
-            //order by rarity
-            sheets.sort(function(a,b){
-                return a.constructor.rarity() - b.constructor.rarity;
-            });
-            _sheets.sort(function(a,b){
-                return a.constructor.rarity() - b.constructor.rarity;
-            });
-            cell.addSheet(sheets);
-            _cell.addSheet(_sheets);
+            let cellC1 = MAGED.Classes.Cell.createNewObject({_x: x, _y:y,_z:z,_h:Math.round(Math.random()*100),_stack:[]});
+            let cellC2 = MAGED.Classes.Cell.createNewObject({_x: _x, _y:_y,_z:_z,_h:Math.round(Math.random()*100),_stack:[]});
+            let cellC3 = MAGED.Classes.Cell.createNewObject({_x: _x, _y:y,_z:((_x + y)*-1),_h:Math.round(Math.random()*100),_stack:[]});
+            let cellC4 = MAGED.Classes.Cell.createNewObject({_x: x, _y:_y,_z:((x + _y)*-1),_h:Math.round(Math.random()*100),_stack:[]});
+            let sheetsC1 = [];
+            let sheetsC2 = [];
+            let sheetsC3 = [];
+            let sheetsC4 = [];
+            addSheets(cArray, sheetsC1);
+            addSheets(cArray, sheetsC2);
+            addSheets(cArray, sheetsC3);
+            addSheets(cArray, sheetsC4);
+
+            cellC1.addSheet(sheetsC1);
+            cellC2.addSheet(sheetsC2);
+            cellC3.addSheet(sheetsC3);
+            cellC4.addSheet(sheetsC4);
             console.log('(' +x+','+y+','+z+')');
         }
     }
